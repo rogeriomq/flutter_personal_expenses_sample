95ab45c6fe1199b9aa63fa034b26f1a78ee709bf
Rogério M. Queiroz <rogerio.mq@gmail.com>
2022-07-04 11:28:21 -0300

    refactor: extract components

--- lib/components/transaction_list.dart
+++ lib/components/transaction_list.dart
@@ -1,6 +1,6 @@
 // ignore_for_file: public_member_api_docs, sort_constructors_first
 import 'package:flutter/material.dart';
-import 'package:intl/intl.dart';
+import 'package:personal_expenses/components/transaction_list_item.dart';
 import 'package:personal_expenses/models/transaction.dart';
 
 class TransactionList extends StatelessWidget {
@@ -40,82 +40,11 @@ class TransactionList extends StatelessWidget {
             itemBuilder: (ctx, index) {
               final currentTransaction = transactions[index];
 
-              return Card(
-                elevation: 5,
-                margin: const EdgeInsets.symmetric(vertical: 8, horizontal: 5),
-                child: ListTile(
-                  leading: CircleAvatar(
-                    radius: 30,
-                    child: Padding(
-                      padding: const EdgeInsets.all(6),
-                      child: FittedBox(
-                          child: Text(currentTransaction.getCurrency())),
-                    ),
-                  ),
-                  title: Text(
-                    currentTransaction.title,
-                    style: Theme.of(context).textTheme.headline6,
-                  ),
-                  subtitle: Text(
-                    DateFormat('d MMM y').format(currentTransaction.date),
-                  ),
-                  trailing: MediaQuery.of(context).size.width > 400
-                      ? TextButton.icon(
-                          onPressed: () => onRemove(currentTransaction.id),
-                          icon: const Icon(Icons.delete),
-                          label: const Text('Delete'),
-                          style: TextButton.styleFrom(
-                              primary: Theme.of(context).errorColor),
-                        )
-                      : IconButton(
-                          icon: const Icon(Icons.delete),
-                          color: Theme.of(context).errorColor,
-                          onPressed: () => onRemove(currentTransaction.id),
-                        ),
-                ),
+              return TransactionListItem(
+                currentTransaction: currentTransaction,
+                onRemove: onRemove,
               );
             },
           );
   }
 }
-
-// Card(
-//   child: Row(
-//     children: [
-//       Container(
-//         margin: const EdgeInsets.symmetric(
-//           horizontal: 15,
-//           vertical: 10,
-//         ),
-//         decoration: BoxDecoration(
-//           border: Border.all(
-//             color: Theme.of(context).primaryColor,
-//             width: 2,
-//           ),
-//         ),
-//         padding: const EdgeInsets.all(10),
-//         child: Text(
-//           currentTransaction.getCurrency(),
-//           style: Theme.of(context).textTheme.headline6,
-//         ),
-//       ),
-//       Column(
-//         crossAxisAlignment: CrossAxisAlignment.start,
-//         children: [
-//           Text(
-//             currentTransaction.title,
-//             style: Theme.of(context).textTheme.headline6,
-//           ),
-//           Text(
-//             DateFormat('d MMM y')
-//                 .format(currentTransaction.date),
-//             style: const TextStyle(
-//                 fontWeight: FontWeight.w400,
-//                 fontSize: 15,
-//                 color: Colors.grey),
-//           ),
-//         ],
-//       ),
-//     ],
-//   ),
-// );
---
+++ lib/components/transaction_list_item.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import 'package:personal_expenses/models/transaction.dart';
+
+class TransactionListItem extends StatelessWidget {
+  const TransactionListItem({
+    Key? key,
+    required this.currentTransaction,
+    required this.onRemove,
+  }) : super(key: key);
+
+  final Transaction currentTransaction;
+  final void Function(String p1) onRemove;
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      elevation: 5,
+      margin: const EdgeInsets.symmetric(vertical: 8, horizontal: 5),
+      child: ListTile(
+        leading: CircleAvatar(
+          radius: 30,
+          child: Padding(
+            padding: const EdgeInsets.all(6),
+            child: FittedBox(child: Text(currentTransaction.getCurrency())),
+          ),
+        ),
+        title: Text(
+          currentTransaction.title,
+          style: Theme.of(context).textTheme.headline6,
+        ),
+        subtitle: Text(
+          DateFormat('d MMM y').format(currentTransaction.date),
+        ),
+        trailing: MediaQuery.of(context).size.width > 400
+            ? TextButton.icon(
+                onPressed: () => onRemove(currentTransaction.id),
+                icon: const Icon(Icons.delete),
+                label: const Text('Delete'),
+                style:
+                    TextButton.styleFrom(primary: Theme.of(context).errorColor),
+              )
+            : IconButton(
+                icon: const Icon(Icons.delete),
+                color: Theme.of(context).errorColor,
+                onPressed: () => onRemove(currentTransaction.id),
+              ),
+      ),
+    );
+  }
+}
